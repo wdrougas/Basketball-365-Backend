94dae210aeec2c18ef923e1fca574332228e5a5f
Will Drougas <wdrougas@gmail.com>
2020-01-30 14:41:25 -0500

    date format changed

--- db/migrate/20200129140545_create_games.rb
+++ db/migrate/20200129140545_create_games.rb
@@ -10,7 +10,7 @@ class CreateGames < ActiveRecord::Migration[6.0]
       t.string :home_team_score
       t.string :arena
       t.string :city
-      t.date :date
+      t.string :date
 
       t.timestamps
     end
