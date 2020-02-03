8e35236e2bae2d4f9af93975c0063b6b1d91a714
Will Drougas <wdrougas@gmail.com>
2020-02-03 16:02:52 -0500

    post user route

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     belongs_to :team
     has_many :comments
+    validates_uniqueness_of :username
+    validates_uniqueness_of :email
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :players
   resources :comments
   resources :games
-  get '/users', to: 'users#index'
+  resources :users
   post '/login', to: 'auth#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
